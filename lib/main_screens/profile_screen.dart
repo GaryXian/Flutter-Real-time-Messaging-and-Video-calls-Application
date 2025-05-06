3c41fb391c5e1df3f3ed2abe5158173220747222
Triet <trietnguyen0902@gmail.con>
2025-05-06 13:30:38 +0700

    update ui with func

--- lib/main_screens/profile_screen.dart
+++ lib/main_screens/profile_screen.dart
@@ -143,3 +143,106 @@ if (confirm == true) {
     );
   }
 }
+
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/settings');
+            },
+            icon: Icon(Icons.settings))
+        ],
+      ),
+
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(width: 10,),
+                  CircleAvatar(
+                    radius: 30,
+                    //backgroundImage: NetworkImage(url),
+                  ),
+                  SizedBox(width: 20,),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            'Name',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 1,),
+                      InkWell(
+                        onTap: () {
+                        },
+                        child: Row(
+                          children: [
+                            Text(
+                              'A longer introduction',
+                              style: TextStyle(
+                                color: Colors.blue,
+                                decoration: TextDecoration.underline,
+                                decorationColor: Colors.blue
+                              )
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 50,),
+            ],
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Informations'),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {},
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.security),
+            title: Text('Manage account'),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {},
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.help),
+            title: Text('Helps and supports'),
+            trailing: Icon(Icons.chevron_right),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
